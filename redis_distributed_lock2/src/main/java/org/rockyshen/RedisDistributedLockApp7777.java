de1691df973ff0d7ea744b6139fe493969af9007
万业PC <rockyshenjunjie@gmail.com>
2024-07-12 09:31:20 +0800

    feat: V8：实现自动续期，后台自定义扫描程序，规定时间业务未完成，自动续期

--- redis_distributed_lock2/src/main/java/org/rockyshen/RedisDistributedLockApp7777.java
+++ redis_distributed_lock2/src/main/java/org/rockyshen/RedisDistributedLockApp7777.java
@@ -6,6 +6,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class RedisDistributedLockApp7777 {
     public static void main(String[] args) {
-        SpringApplication.run(RedisDistributedLockApp7777.class, args);
+        SpringApplication.run(RedisDistributedLockApp7777.class);
     }
 }
